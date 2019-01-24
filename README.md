8ad671dd4a7b3569129b4dc63de9443f632fd1ae
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-01-24 10:21:33 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-camera
+# camera
+
+
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+IoT=github:DoraLC/pxt-camera
+```
---
+++ main.ts
@@ -0,0 +1,10 @@
+//%color=#0B0B61 icon="\uf1eb" block="Camera"
+namespace camera{
+
+    //% shim=serialBuffer::setSerialBuffer
+    function setSerialBuffer(size: number): void {
+        return null;
+    }
+
+    setSerialBuffer(120)
+}
